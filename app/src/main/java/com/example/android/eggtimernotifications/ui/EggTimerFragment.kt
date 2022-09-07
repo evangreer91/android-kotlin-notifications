4885cd0855638183b26dbb1d49b1ef6b83b26746
Evan Greer <evan.greer@iterable.com>
2022-09-07 09:50:42 -0600

    updates notification priority to high

--- app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
@@ -68,8 +68,8 @@ class EggTimerFragment : Fragment() {
             val notificationChannel = NotificationChannel(
                 channelId,
                 channelName,
-                // TODO: Step 2.4 change importance
-                NotificationManager.IMPORTANCE_LOW
+                // change importance to high to alert user when app is in foreground
+                NotificationManager.IMPORTANCE_HIGH
             )
             // TODO: Step 2.6 disable badges for this channel
 
